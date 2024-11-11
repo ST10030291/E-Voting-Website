1fe394da0c51d6dbc59bd9d67572b496fbc64171
ST10030291 <ST10030291@vcconnect.edu.za>
2024-11-11 14:50:05 +0200

    Start E-Voting Website project

---
+++ E-Voting Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Voting_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
