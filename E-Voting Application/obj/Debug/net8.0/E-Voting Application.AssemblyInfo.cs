b3a195c8a143ad6dd0a4d5f6ca455c238c62b61e
ST10030291 <ST10030291@vcconnect.edu.za>
2024-11-15 20:25:54 +0200

    Updated Home Page

--- E-Voting Application/obj/Debug/net8.0/E-Voting Application.AssemblyInfo.cs
+++ E-Voting Application/obj/Debug/net8.0/E-Voting Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Voting Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc311025cc0c4e7d0bc6b25fdd9b1c4d77be6d36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b00ee4a9318920901496f8bf5f82b6d6015d438d")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Voting Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Voting Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
